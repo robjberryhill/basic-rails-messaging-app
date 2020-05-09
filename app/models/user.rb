57eaedc839d311461b2fa5368e564b658ffe2e5a
robjberryhill <robjberryhill@gmail.com>
2020-05-08 17:00:58 -0700

    Added messages resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        length: { miniumu: 3, maximum: 25 }
+  has_many :messages
   has_secure_password
 end
